7a3ec3c8d9c77e17b57a023e7bc464cce5e707e6
Victoria Landron <viclandron@gmail.com>
2020-09-05 17:20:56 -0400

    added coloring to time slots

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ var timeDisplay = $("#currentDay");
 var today = moment();
 timeDisplay.text(today.format("dddd, MMMM Do YYYY"));
 
-//coloring the tasks
-$(".description").each(function(){
+function loadTasks() {
+    textContent = JSON.parse(localStorage.getItem("tasks"));
+};
 
-})
+//coloring the tasks
+function timeMatch() {
+    $(".description").each(function(){
+        var currentHour = parseInt(moment().format('H'));
+        console.log(currentHour)
+        var rowHour = $(this).attr("id")
+        console.log(rowHour);
+        var rowNumber = parseInt(rowHour);
+        console.log(rowNumber);
+
+        if (rowNumber === currentHour) {
+            $(this).addClass("present");
+        }
+       else if (rowNumber < currentHour) {
+            $(this).addClass("past");
+        }
+        else {
+            $(this).addClass("future");
+        }; 
+    });
+    debugger;
+};
 
 //detect when user clicks save
 $("button").click(function(){
@@ -16,4 +38,8 @@ $("button").click(function(){
     localStorage.setItem("tasks", JSON.stringify(textContent));
 });
 
-//load tasks
+//load tasks on refresh
+loadTasks();
+
+//run time match on refresh
+timeMatch();
