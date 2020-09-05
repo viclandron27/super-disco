c71e9b5e0a141b40185b22e66ed7074662d141c8
Victoria Landron <viclandron@gmail.com>
2020-09-05 16:30:37 -0400

    save text input to local storage

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+var tasks = {};
+
 //show current date
 var timeDisplay = $("#currentDay");
 var today = moment();
 timeDisplay.text(today.format("dddd, MMMM Do YYYY"));
+
+//coloring the tasks
+$(".description").each(function(){
+
+})
+
+//detect when user clicks save
+$("button").click(function(){
+    var textContent = $(".description").val().trim();
+    localStorage.setItem("tasks", JSON.stringify(textContent));
+});
+
+//load tasks
